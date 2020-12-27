f34d2217b1e8f09b1a70646b7553223ccb80c121
Kevin Murphy <murphy.kevin.mail@gmail.com>
2020-12-27 13:01:46 -0500

    connect neovim config to vim config

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ brew bundle
 ln -sv "$PWD/.aliases" "$HOME"
 ln -sv "$PWD/.zshrc" "$HOME"
 
+mkdir -p "$HOME/.config/nvim/"
+ln -sv "$PWD/init.vim" "$HOME/.config/nvim"
+
 ln -sv "$PWD/.gitconfig" "$HOME"
 ln -sv "$PWD/.gitmessage" "$HOME"
